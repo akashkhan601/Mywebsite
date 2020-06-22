6e6bca5c460b0328a3a41ede7960862522af7277
Akash <akakashkhan857@gmail.com>
2020-06-22 10:59:08 +0600

    my upadte full web code

---
+++ update code/fast.js
@@ -0,0 +1,31 @@
+
+window.addEventListener("scroll",function(){
+  var myHeader = document.querySelector("header");
+  myHeader.classList.toggle("sticky", window.scrollY>80);
+})
+
+
+var slideIndex = 0;
+autoSlider();
+
+function autoSlider(){
+  var slides = document.getElementsByClassName("myslider");
+  for(var i = 0; i < slides.length;i++){
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if(slideIndex>slides.length){
+  slideIndex = 1;
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(autoSlider, 2000);
+}
+
+
+
+
+
+
+
+
+
