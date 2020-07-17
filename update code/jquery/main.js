6d6c9b71307db010a1382cb913e3ac897eebe957
Akash <akakashkhan857@gmail.com>
2020-07-17 21:59:30 +0600

    testimonial added

--- update code/jquery/main.js
+++ update code/jquery/main.js
@@ -22,6 +22,20 @@ $(document).ready(function(){
             enabled:true
           }
 });
+
+$(".owl-carousel").owlCarousel({
+    items :1,
+    loop :true,
+     nav  : true,
+    dots  :true,
+    autoplay: true,
+    autoplayTimeout:5000,
+    autoplaySpeed :2000,
+    navSpeed      : 1000,
+     dotsSpeed     : 2000,
+     navText       : ['<i class="fas   fa-arrow-left"></i> ' , '   <i class="fas  fa-arrow-right"></i> ']
+    
+});
     
 });
 
